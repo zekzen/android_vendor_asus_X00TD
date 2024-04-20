59b7a5dfaf0cee99bee21b9f66cd4933788c4fdc
dotkit <ewprjkt@proton.me>
2024-04-20 10:41:35 +0000

    sdm660-common: Drop QTI datastatusnotification
    Change-Id: I287f067a7a346d92988f71fbc813e0f63857450d

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -763,7 +763,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
